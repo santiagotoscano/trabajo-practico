5882bd2a979b4724f5e079f82de2608a0e16687e
santiagotoscano <santiago.toscano@letflow.com.ar>
2019-09-18 19:38:02 -0300

    Fix request method

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.post('/ML-WEBHOOK', function (req, res) {
 
   let meliObject = new meli.Meli(4288953061163822, 'o79fq80g3NXuS1hiPUUs17zToROdeou2');
   meliObject.refreshAccessToken(() => {
-    request({uri: `https://api.mercadolibre.com${req.body.resource}`, method: "GET", json: true}, (err, res, body) => console.log(body))
+    request(`https://api.mercadolibre.com${req.body.resource}`, {method: "GET", json: true}, (err, res, body) => console.log(body))
   });
 
   return res.send({ error: false, message: 'hola' });
