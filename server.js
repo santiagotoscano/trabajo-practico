cb4a2c75de7b8bf9cd890448ede7c0c431c5d986
santiagotoscano <santiago.toscano@letflow.com.ar>
2019-09-18 20:06:18 -0300

    Try meli objects v3

--- server.js
+++ server.js
@@ -109,10 +109,15 @@ app.post('/ML-WEBHOOK', function (req, res) {
 
   let meliObject = new meli.Meli(4288953061163822, 'o79fq80g3NXuS1hiPUUs17zToROdeou2');
 
+  console.log(req.body)
+
   meliObject.refreshAccessToken(() => {
     meliObject.get(req.body.resource, (err, res) => {
-      meliObject.post('/answers', {question_id: res.body.id, text: `Respuesta de pregunta $${res.body.text}`}, () => {
-        console.log('done')
+
+      console.log(res)
+      meliObject.post('/answers', {question_id: res.body.id, text: `Respuesta de pregunta ${res.body.text}`}, (err, res) => {
+
+        console.log(res.body)
       })
     });
 
